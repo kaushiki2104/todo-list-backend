70e5d6d62e419dfa4349ac93c097b82e0b165e8a
kaushiki2104 <137433922+kaushiki2104@users.noreply.github.com>
2025-09-14 15:08:31 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,23 +1,40 @@
 const mongoose = require('mongoose');
 const colors = require('colors');
 let isConnected;
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 const connectDB = async () => {
- 
-  if (isConnected) {
-    console.log("MongoDB already connected");
-    return;
+
+   if (cached.conn) {
+    return cached.conn;
   }
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URL_LOCAL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URL_LOCAL).then((conn) => {
+      return conn;
     });
-    isConnected = conn.connections[0].readyState;
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error("MongoDB Error:", error.message);
-    process.exit(1);
   }
+  cached.conn = await cached.promise;
+  return cached.conn;
+
+
+ // second 
+  // if (isConnected) {
+  //   console.log("MongoDB already connected");
+  //   return;
+  // }
+  // try {
+  //   const conn = await mongoose.connect(process.env.MONGO_URL_LOCAL, {
+  //     useNewUrlParser: true,
+  //     useUnifiedTopology: true,
+  //   });
+  //   isConnected = conn.connections[0].readyState;
+  //   console.log(`MongoDB Connected: ${conn.connection.host}`);
+  // } catch (error) {
+  //   console.error("MongoDB Error:", error.message);
+  //   process.exit(1);
+  // }
 
 
   // try {
@@ -38,3 +55,4 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
